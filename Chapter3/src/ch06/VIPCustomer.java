bd459cdd7f85eecdf3f562db650f42360c1bbd69
jaeeunhyun <rosa2070@naver.com>
2021-04-22 23:21:15 +0900

    dfsfd

---
+++ Chapter3/src/ch06/VIPCustomer.java
@@ -0,0 +1,49 @@
+package ch06;
+
+public class VIPCustomer extends Customer{
+
+	
+
+
+	double salesRatio;
+	private String agendID;
+	
+/*	public VIPCustomer() {
+		
+		super(0, "no-name");
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		customerGrade = "VIP";
+		
+		System.out.println("VIPCustomer() call");
+
+	}*/
+	
+	public VIPCustomer(int customerID, String customerName) {
+		super(customerID, customerName);
+
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		
+	}
+	
+	
+	
+	
+	
+	@Override
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		price -= (int)(price * salesRatio);
+		return price;
+	}
+
+
+
+
+
+	public String getAgentID() {
+		return agendID;
+	}
+}
